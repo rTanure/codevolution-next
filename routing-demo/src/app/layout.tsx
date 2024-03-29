993328674cb4ecd97883ff46927838d912e02c11
Richardy Tanure <richardy.tanure@hotmail.com>
2024-03-28 22:58:10 -0300

    finished 14 - Layouts

--- routing-demo/src/app/layout.tsx
+++ routing-demo/src/app/layout.tsx
@@ -10,7 +10,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header><p>Header</p></header>
+
+        {children}
+
+        <footer><p>Footer</p></footer>
+      </body>
     </html>
   )
 }
