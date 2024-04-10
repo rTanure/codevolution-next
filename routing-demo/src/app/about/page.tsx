5589592609c5e22e498e31957e91fab3c7f2c3ae
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-10 10:37:19 -0300

    finished: 17 - Routing Metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next/types"
+
+// Exporting a metadata object
+export const metadata: Metadata = {
+  title: "About Codevolution",
+}
+
 export default function About() {
   return <h1>About this app</h1>
 }
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,7 +1,23 @@
-export default function ProductDetail({ params }: {
+import { Metadata } from "next"
+
+type Props = {
   params: {
     productId: string
   }
-}) {
+}
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  // Fetch API
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`IPhone ${params.productId}`)
+    },1000)
+  })
+  return {
+    title: `product ${title}`
+  }
+}
+
+export default function ProductDetail({ params }: Props) {
   return <h1>Details about product {params.productId}</h1>
 }
