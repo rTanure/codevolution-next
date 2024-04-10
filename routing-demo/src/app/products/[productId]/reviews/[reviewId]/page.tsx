744a1a42e09e8f512af880b8c4cbe72c0a69ecb3
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-10 16:03:43 -0300

    24 - Error Handling

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,22 @@
 import { notFound } from "next/navigation"
 
-export default function ProductReviewPage({params}: {
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count)
+}
+
+export default function ProductReviewPage({
+  params
+}: {
   params: {
     productId: string
     reviewId: string
   }
 }) {
+  const random = getRandomInt(2)
+  if(random == 1) {
+    throw new Error("Error loading review2")
+  }
+
   if (parseInt(params.reviewId) > 1000) notFound()
   return <h1>Review {params.reviewId} for product {params.productId}</h1>
 }
