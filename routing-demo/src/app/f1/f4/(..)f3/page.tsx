5386268ce829eb3786c7d53819c589526a3b7f70
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-13 21:40:11 -0300

    finished: 31 - Intercepting Routes

---
+++ routing-demo/src/app/f1/f4/(..)f3/page.tsx
@@ -0,0 +1,7 @@
+export default function IntercepdetF3() {
+  return (
+    <div>
+      <h1>Intercepted F3</h1>
+    </div>
+  )
+}
---
+++ routing-demo/src/app/f1/f4/(...)about/page.tsx
@@ -0,0 +1,7 @@
+export default function InterceptedAbout() {
+  return (
+    <div>
+      <h1>Intercepted About</h1>
+    </div>
+  )
+}
