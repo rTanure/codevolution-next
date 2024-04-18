c7f5ab108eadf982dcef09a3ac2a57b65f2b404f
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-18 16:30:02 -0300

    finished: 42 - Cookies in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,16 +1,23 @@
 import { type NextRequest } from "next/server"
-import { headers } from "next/headers"
+import { headers, cookies } from "next/headers"
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
   const headerList = headers()
 
+  const theme = request.cookies.get("theme")
+  console.log(theme)
+
+  cookies().set('resultsPerPage', '20')
+  console.log(cookies().get('resultsPerPage'))
+
   console.log(requestHeaders.get("Authorization"))
-  console.log(headerList)
+  // console.log(headerList)
 
   return new Response("<h1>Profile response</h1>", {
     headers: {
-      "Content-Type": "text/html"
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark"
     }
   })
 }
