508962f54296de26b0d430c8dc366cffa92d3d18
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-18 16:19:55 -0300

    finished: 41 - Headers in Route Handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,16 @@
-export async function GET() {
-  return new Response("Profile response")
+import { type NextRequest } from "next/server"
+import { headers } from "next/headers"
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers)
+  const headerList = headers()
+
+  console.log(requestHeaders.get("Authorization"))
+  console.log(headerList)
+
+  return new Response("<h1>Profile response</h1>", {
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
 }
