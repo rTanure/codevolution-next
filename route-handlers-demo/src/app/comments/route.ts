d285570ed8090e676b2653700c07d19b120e9402
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-18 14:41:00 -0300

    finished: 34 - Handling POST Request

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -3,3 +3,25 @@ import { comments } from "./data"
 export async function GET() {
   return Response.json(comments)
 }
+
+export async function POST(request: Request) {
+  const comment = await request.json()
+  console.log(comment)
+
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text
+  }
+
+  comments.push(newComment)
+  return new Response(
+    JSON.stringify(newComment), 
+    {
+      headers: {
+        "Content-type": "application/json"
+      },
+      status: 201 // New resource created
+    }
+  )
+
+}
