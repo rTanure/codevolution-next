03a80e68d50fb7ce6a5f6280059ad2c20ee23a7a
Richardy Tanure <richardy.tanure@hotmail.com>
2024-04-18 15:44:53 -0300

    finished: 40 - Redirects in Route Handlers

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,10 +1,13 @@
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(
   _request: Request, 
   { params }: { params: { id: string } }
 ) {
+  
   const comment = comments.find((comment) => comment.id === parseInt(params.id))
+  if(!comment) redirect("/comments")
   return Response.json(comment)
 }
 
